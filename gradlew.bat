0b7aabd984043e47e965eaf74eee48e6b7f847e5
Likhit Dharmapuri <likhitd1992@gmail.com>
2018-12-03 23:39:09 -0800

    Adding build dependency on Memcached-Java-Client.

--- gradlew.bat
+++ gradlew.bat
@@ -65,6 +65,11 @@ set CMD_LINE_ARGS=%*
 
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
+@rem Build Memcached-Java-Client
+cd ./Memcached-Java-Client
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
+cd ..
+
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
 
